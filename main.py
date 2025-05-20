1107b6608fdd329b2277b68ae15dd2d87ef814c6
IronhawkReigns <yejoons_2026@gatech.edu>
2025-05-20 14:14:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -175,4 +175,8 @@ def write_combined_summary(all_summaries):
     if cell_updates:
         worksheet.batch_update(cell_updates)
 
+    # After writing new data, clear static header rows (2, 8, 9, 16, 17, 26)
+    for row_num in [2, 8, 9, 16, 17, 26]:
+        worksheet.update(f"B{row_num}", [[""]])
+
 write_combined_summary(all_summaries)
